d1d2e50f5363b963b0771f93a937f2dfb20b7689
Jak <jaksawesome10@github.com>
2018-09-27 21:41:07 -0400

    9/27 homework

--- Car.java
+++ Car.java
@@ -1,11 +1,46 @@
+/* This is a program used to make digital cars
+ * John Khachian
+ * 9/27
+ */
 
 public class Car{
-
+ 
+ //fields
+ 
+ String model;
+ 
+ int year;
+ 
+ //Constructor
+ 
+ public Car(String m, int y){
   
+  model = m;
   
-  public Car(){
+  year = y;
   
+ }
+ 
+ //methodd
+ 
+ //Accessors
+ 
+ public String getModel(){return model;}
+ 
+ public int getYear(){return year;}
+ 
+ //toString
+ 
+ public String toString() {
   
-  }
-
+  return model + ", " + year;
+  
+ }
+ 
+ //mutators
+ 
+ public void changeModel(String m) {model = m;}
+ 
+ public void changeYear(int y) {year = y;}
+ 
 }
---
+++ OurCars.java
@@ -0,0 +1,21 @@
+
+/* This is a program used to organize digital cars
+ * John Khachian
+ * 9/27
+ */
+
+public class OurCars{
+ 
+ public static void main(String [] args) {
+  
+  Car john = new Car("Tesla model s", 2015);
+  
+  Car christian = new Car("Ford Fusion", 2003);
+    
+  System.out.println(john);
+  
+  System.out.println(christian);
+  
+ }
+ 
+}
